434ad56fa17cda72cea6c8322fe2a1a67b203834
dtun <dtun@me.com>
2024-08-01 07:47:25 -0700

    Add data

---
+++ data/index.ts
@@ -0,0 +1,37 @@
+export let menuData = [
+  {
+    id: "1",
+    icon: "map-outline",
+    title: "Pull Requests",
+  },
+  {
+    id: "2",
+    icon: "star-outline",
+    title: "Starred",
+  },
+  {
+    id: "3",
+    icon: "alert-circle-outline",
+    title: "Issues",
+  },
+  {
+    id: "4",
+    icon: "chatbox-outline",
+    title: "Discussions",
+  },
+  {
+    id: "5",
+    icon: "apps-outline",
+    title: "Repositories",
+  },
+  {
+    id: "6",
+    icon: "home-outline",
+    title: "Organizations",
+  },
+  {
+    id: "7",
+    icon: "map-outline",
+    title: "Projects",
+  },
+];
