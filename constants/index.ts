a0f234c5fc88801c465cf20f62886f06da2ec282
dtun <dtun@me.com>
2024-08-04 07:09:26 -0700

    Add constants

---
+++ constants/index.ts
@@ -0,0 +1,33 @@
+export const tintColorLight = "#2f95dc";
+export const tintColorDark = "#fff";
+export const red = "#e74c3c";
+
+export let colors = {
+  light: {
+    text: "#1A1A1A",
+    background: "#fff",
+    tint: tintColorLight,
+    tabIconDefault: "#ccc",
+    tabIconSelected: tintColorLight,
+    icon: "#333333",
+    listContainer: "#F5F5F5",
+  },
+  dark: {
+    text: "#F0F0F0",
+    background: "#1E1E1E",
+    tint: tintColorDark,
+    tabIconDefault: "#ccc",
+    tabIconSelected: tintColorDark,
+    icon: "#E0E0E0",
+    listContainer: "#121212",
+  },
+};
+
+export let sliderTheme = {
+  disableMinTrackTintColor: "#808080", // Medium gray
+  maximumTrackTintColor: "#808080", // Medium gray
+  minimumTrackTintColor: red, // Red
+  cacheTrackTintColor: "#A9A9A9", // Dark gray
+  bubbleBackgroundColor: red, // Red
+  heartbeatColor: red, // Red
+};
